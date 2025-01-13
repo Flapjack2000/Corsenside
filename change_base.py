ef3c9cdc6751952d6ee76f1e8bcb657ab0965315
Flapjack2000 <148223049+Flapjack2000@users.noreply.github.com>
2025-01-13 17:03:20 -0500

    Add change_base.py

---
+++ change_base.py
@@ -0,0 +1,36 @@
+
+def change_base(num: int, base: int):
+    digit_table: dict = {
+        10: "a", 11: "b", 12: "c", 13: "d", 14: "e", 15: "f", 16: "g", 17: "h", 18: "i", 19: "j", 20: "k",
+        21: "l", 22: "m", 23: "n", 24: "o", 25: "p", 26: "q", 27: "r", 28: "s", 29: "t", 30: "u", 31: "v",
+        32: "w", 33: "x", 34: "y", 35: "z", 36: "A", 37: "B", 38: "C", 39: "D", 40: "E", 41: "F", 42: "G",
+        43: "H", 44: "I", 45: "J", 46: "K", 47: "L", 48: "M", 49: "N", 50: "0", 51: "P", 52: "Q", 53: "R",
+        54: "S", 55: "T", 56: "U", 57: "V", 58: "W", 59: "X", 60: "Y", 61: "Z", 62: "!", 63: "@", 64: "#",
+        65: "$", 66: "%", 67: "^", 68: "&", 69: "*", 70: "(", 71: ")", 72: "_", 73: "-", 74: "+", 75: "=",
+        76: "`", 77: "~", 78: "[", 79: "]", 80: "{", 81: "}", 82: "|", 83: "/", 84: "\\", 85: "'", 86: '"',
+        87: ",", 88: ".", 89: "<", 90: ">", 91: "?", 92: "Δ", 93: "µ", 94: "∑", 95: "δ", 96: "β", 97: "Ω",
+        98: "ϴ", 99: "π"}
+
+    upper_limit: int = max(digit_table.keys()) + 1
+
+    if base < 2 or base > upper_limit:
+        raise ValueError(f"Base must be between 2 and {upper_limit}.")
+
+    if num % 1:
+        raise ValueError(f"Number must be an integer.")
+
+    remainders: list = []
+    while num != 0:
+        remainder: int = num % base
+
+
+        remainder: str = digit_table[remainder] if remainder in digit_table else remainder
+
+        remainders.append(str(remainder))
+        num //= base
+
+    remainders.reverse()
+
+    return ''.join(remainders)
+
+print(change_base(5472507324895732409532, 100))
