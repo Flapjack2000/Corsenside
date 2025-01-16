e51f72e66d79e1f731fc17acc0d26b3cd880ba5f
Flapjack2000 <148223049+Flapjack2000@users.noreply.github.com>
2025-01-15 23:24:52 -0500

    Update problem2.py

--- Project Euler/problem2.py
+++ Project Euler/problem2.py
@@ -1,4 +1,5 @@
 """
+Zachary Williams
 Problem 2: Even Fibonacci Numbers
 By considering the terms in the Fibonacci sequence whose values
 do not exceed four million, find the sum of the even-valued terms.
