b377f4ad3fb073efcb9bf76a6594678843a74d73
Flapjack2000 <148223049+Flapjack2000@users.noreply.github.com>
2025-01-15 23:21:18 -0500

    Create problem3.py

---
+++ Project Euler/problem3.py
@@ -0,0 +1,36 @@
+"""
+Problem 3: Largest Prime Factor
+What is the largest prime factor of the number 600851475143?
+
+Example:
+The prime factors of 13195 are 5, 7, 13, and 29.
+"""
+import math
+
+def list_factors(n) -> list:
+    """ Find all factors of a number n, including 1 and n. """
+
+    # check for factors under sqrt(n)
+    # (not n/i % 1) means (n/i % 1 == 0)
+    factors: list = [i for i in range(1, int(n ** 0.5) + 1) if not n / i % 1]
+
+    # if (i) is a factor, then (n/i) must also be a factor
+    # don't add duplicates from perfect squares
+    factors.extend([int(n / f) for f in factors if (not n / f % 1) and (n / f not in factors)])
+
+    return sorted(factors)
+
+def check_prime(n) -> bool:
+    """ Check if a number n is prime. """
+    return True if len(list_factors(n)) == 2 else False
+        
+def list_prime_factors(n) -> list:
+    """ Find all prime factors of a number n, including 1 and n (if n is prime). """
+    return [f for f in list_factors(n) if check_prime(f)]
+
+def largest_prime_factor(n) -> int:
+    return max(list_prime_factors(n))
+
+if __name__ == '__main__':
+    answer = largest_prime_factor(600851475143)
+    print(answer)
